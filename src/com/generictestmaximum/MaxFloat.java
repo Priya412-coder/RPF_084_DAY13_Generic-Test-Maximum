0f6cac7b7e42aeb197f4b386374c6933e8d59a91
Priya412-coder <priyankasingh8249@gmail.com>
2021-12-17 00:10:12 +0530

    adding generic problem

--- src/com/generictestmaximum/MaxFloat.java
+++ src/com/generictestmaximum/MaxFloat.java
@@ -1,7 +1,7 @@
 package com.generictestmaximum;
 
 public class MaxFloat {
-	public Double FindMaxNumber(Double inputOne, Double inputTwo, Double inputThree) {
+	public static Float FindMaxNumber(Float inputOne, Float inputTwo, Float inputThree) {
 		if (inputOne.compareTo(inputTwo) > 0 && inputOne.compareTo(inputThree) > 0) {
 			return inputOne;
 		} else if (inputTwo.compareTo(inputOne) > 0 && inputTwo.compareTo(inputThree) > 0) {
--- src/com/generictestmaximum/MaxString.java
+++ src/com/generictestmaximum/MaxString.java
@@ -1,7 +1,7 @@
 package com.generictestmaximum;
 
 public class MaxString {
-	public String FindMaxString(String str1, String str2, String str3) {
+	public static String FindMaxString(String str1, String str2, String str3) {
 		if (str1.compareTo(str2) > 0 && str1.compareTo(str3) > 0) {
 			return str1;
 		} else if (str3.compareTo(str1) > 0 && str3.compareTo(str2) > 0) {
